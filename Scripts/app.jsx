81ce617401a75ba0793e7d536e1573e3dc9af953
Ray H.J Kwon <nicekhj@gmail.com>
2015-01-11 22:47:21 +1100

    added popover for details button

--- Scripts/app.jsx
+++ Scripts/app.jsx
@@ -40,32 +40,6 @@
 	  }
 	});
 
-  App.UserView = React.createClass({
-    render() {
-      var detailsUrl = "/users/" + this.props.user.Id;
-      return (
-        <tr>
-          <td>{this.props.user.Id}</td>
-          <td>{this.props.user.Name}</td>
-          <td>{this.props.user.Suburb}</td>
-          <td>
-            <button className="btn btn-primary" onClick={this.deleteUser}>Delete</button>&nbsp;
-            <a href={detailsUrl} className="btn btn-primary" onClick={this.moveToDetailsPage}>Details</a>
-          </td>
-        </tr>
-      );
-    },
-
-    deleteUser(event){
-      var confirmed = confirm("Delete " + this.props.user.Name + " living in " + this.props.user.Suburb + "?");
-      this.props.deleteUser(this.props.user.Id);
-    },
-
-    moveToDetailsPage(e) {
-      App.router.navigate(e.currentTarget.pathname, {trigger: true});
-    }
-  });
-
   App.UsersView = React.createClass({
     getInitialState() {
       return {users: this.props.users};
@@ -93,6 +67,38 @@
         return user.Id !== userId;
       });
       this.setState({users: users});
+    }
+  });
+
+  App.UserView = React.createClass({
+    render() {
+      var detailsUrl = "/users/" + this.props.user.Id;
+      var popoverMessage = this.props.user.Name + "님의 상세 정보를 보실수 있습니다";
+      return (
+        <tr>
+          <td>{this.props.user.Id}</td>
+          <td>{this.props.user.Name}</td>
+          <td>{this.props.user.Suburb}</td>
+          <td>
+            <button className="btn btn-primary" onClick={this.deleteUser} data-loading-text="Loading...">Delete</button>&nbsp;
+            <a href={detailsUrl} className="btn btn-primary" onClick={this.moveToDetailsPage} data-toggle="popover" title="React" data-content={popoverMessage}>Details</a>
+          </td>
+        </tr>
+      );
+    },
+
+    componentDidMount() {
+      $('[data-toggle="popover"]').popover({trigger:'hover'});
+    },
+
+    deleteUser(event){
+      var confirmed = confirm("Delete " + this.props.user.Name + " living in " + this.props.user.Suburb + "?");
+      if (confirmed)
+        this.props.deleteUser(this.props.user.Id);
+    },
+
+    moveToDetailsPage(e) {
+      App.router.navigate(e.currentTarget.pathname, {trigger: true});
     }
   });
 
